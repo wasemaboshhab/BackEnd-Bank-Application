12a20b00fb73405c8d5dc0abf2c1f93f43a91bb0
WasimShhab <82174159+wasemaboshhab@users.noreply.github.com>
2024-05-07 18:33:58 +0300

    Initial commit

---
+++ src/main/java/My/Project/BackEnd/Bank/Application/BackEndBankApplication.java
@@ -0,0 +1,13 @@
+package My.Project.BackEnd.Bank.Application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEndBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/My/Project/BackEnd/Bank/Application/BackEndBankApplicationTests.java
@@ -0,0 +1,13 @@
+package My.Project.BackEnd.Bank.Application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
